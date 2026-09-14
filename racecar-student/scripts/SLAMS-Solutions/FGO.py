6513d7d127f41594a84884883ae41cd86dec8307
Viir Sood <viirnsood@gmail.com>
2026-09-14 19:11:16 -0400

    Log: implemented main loop for graph optimizer, iteration still to be implemented

--- racecar-student/scripts/SLAMS-Solutions/FGO.py
+++ racecar-student/scripts/SLAMS-Solutions/FGO.py
@@ -2,13 +2,24 @@
 import numpy as np
 class FactorGraphOptimizer:
     trajectory = []
-    # Each element of edge with key i contains [t, z_i, omega_i]
+    # Each element of edge with key i contains [t, z_i, omega_i y]
+    # y == 0 means that the current factor edge is an odometry estimate, while 1 is ICP
+    # y helps determine which jacobian to choose
     edges = {}
+    num_edges = 0
+    x_t = 0
     def __init__(self, x0):
         self.trajectory.append(x0)
         self.trajectory.append(0)
-    def update(self, newEdges):
-        pass
+    def update(self, z_icp, z_imu, omega_icp, omega_imu):
+        #determine delta x, a vector of increments that is made
+        #to the trajectory. 
+        num_edges += 1
+        self.edges[num_edges] = np.array([self.x_t, z_icp, omega_icp, 1]) 
+        num_edges += 1
+        self.edges[num_edges] = np.array([self.x_t, z_imu, omega_imu, 0]) 
+        self.x_t += 1
+                
     def edgeCost(self, edge):
         nextNode = self.edges[edge]
         residual = self.trajectory[nextNode[0]+1] - (self.trajectory[nextNode[0]] + nextNode[1])
